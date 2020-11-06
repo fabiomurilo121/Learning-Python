23ff7155e3c8ab1cfb88c7fcf8ea48449c414236
fabio <fabio.cagni@gmail.com>
2020-11-06 14:22:46 -0300

    inicio

---
+++ ex009.py
@@ -0,0 +1,19 @@
+string = " hello  "
+
+print(string.strip())
+
+#collection is ordered, changeable, and allows duplicate members
+list = ["apple", "banana", "cherry"]
+print(type(list))
+
+tuple = ("apple", "banana", "cherry")
+print(type(tuple))
+
+# doesn't allow duplicate elements
+set = {"apple", "banana", "cherry", "banana"}
+print(set)
+print(type(set))
+
+dictionary = {"name": "apple", "color": "green"}
+print(type(dictionary))
+
---
+++ ex010.py
@@ -0,0 +1,10 @@
+
+
+lista = [1,2,3,4,6,7,8,9,7,10]
+
+def tiraRepeticao(entrada):
+    for i in lista:
+        print(i)
+
+
+tiraRepeticao(lista)
